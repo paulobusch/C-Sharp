b21efa5bf0f56e8d03f8a571ad4f0ae5306929c9
Paulo <paulo202015@outlook.com.br>
2018-03-24 22:44:34 -0300

    [IF - Condicional] -> Converção Toint32

---
+++ IF - Condicional.cs
@@ -0,0 +1,10 @@
+using System;
+namespace IF{
+	class Programa{
+		static void Main(String[] args){
+			int n = 0;
+			Console.WriteLine("Digite um número: ");
+			n = Convert.Toint32(Console.ReadLine());
+		}
+	}
+}
