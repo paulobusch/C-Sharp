1e87b5dc128e8393d96a0736677092f1ceceec2b
Paulo <paulo202015@outlook.com.br>
2018-03-25 00:06:24 -0300

    [Operadores Relacionais] -> Igualdade

--- Operadores Relacionais.cs
+++ Operadores Relacionais.cs
@@ -2,7 +2,11 @@ using System;
 namespace operadores_relacionais{
 	class Programa{
 		static void Main(String[] args){
-			
+			int x, y = 0;
+			Console.WriteLine("Digite um número: ");
+			x = Convert.ToInt32(Console.ReadLine());
+			bool b = (x==y);
+			Console.WriteLine("\nY = 0\n\nX == Y: " + b);
 		}
 	}
 }
