9ed88644b83d287ce8a35aa7789eb3452b9ab47a
Paulo <paulo202015@outlook.com.br>
2018-03-24 21:57:14 -0300

    Estrutura básica

---
+++ Calculadora.cs
@@ -0,0 +1,11 @@
+using System;
+namespace Calculadora{
+	class Programa{
+		static void Main(String[] args){
+			int n1, n2;
+			Console.WriteLine("Digite um numero numero: ");
+			n1 = Console.ReadLine();
+
+		}
+	}
+}
