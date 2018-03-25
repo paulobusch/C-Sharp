e074f651389543091b4828cc4ae946552bf2f3ae
Paulo <paulo202015@outlook.com.br>
2018-03-24 23:30:25 -0300

    [IF - Condicional] -> Adaptação final

--- IF - Condicional.cs
+++ IF - Condicional.cs
@@ -3,12 +3,13 @@ namespace IF{
 	class Programa{
 		static void Main(String[] args){
 			int n = 0;
+			string t = "\nO número digitado é ";
 			Console.WriteLine("Digite um número: ");
 			n = Convert.ToInt32(Console.ReadLine());
 			if(n > 0){
-				Console.WriteLine("\n\nO número digitado é positivo\n\n");
+				Console.WriteLine(t + "positivo\n\n");
 			}else{
-				Console.WriteLine("\n\nO número digitado é negativo\n\n");
+				Console.WriteLine(t + "negativo\n\n");
 			}
 		}
 	}
