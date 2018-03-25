5d465f05e70fdb2630f060f44ddd7a5def4dbd35
Paulo <paulo202015@outlook.com.br>
2018-03-24 22:00:03 -0300

    Soma

--- Calculadora.cs
+++ Calculadora.cs
@@ -5,7 +5,9 @@ namespace Calculadora{
 			int n1, n2;
 			Console.WriteLine("Digite um numero numero: ");
 			n1 = Console.ReadLine();
-
+			Console.Write("Digite outro número");
+			n2 = Console.ReadLine();
+			console.WriteLine("A soma vale: " + n1+n2);
 		}
 	}
 }
