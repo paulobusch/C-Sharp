9ee4d7441cd40d869b43ae8072105f5151bb7ff8
Paulo <paulo202015@outlook.com.br>
2018-03-24 22:12:56 -0300

    Subtração

--- Calculadora.cs
+++ Calculadora.cs
@@ -2,7 +2,7 @@ using System;
 namespace Calculadora{
 	class Programa{
 		static void Main(String[] args){
-			float n1, n2, soma;
+			float n1, n2, operacao;
 			string tmp; //valor temporário de leitura
 			Console.WriteLine("Digite um numero numero: ");
 			tmp = Console.ReadLine();//realiza leitura
@@ -10,8 +10,10 @@ namespace Calculadora{
 			Console.Write("Digite outro número: ");
 			tmp = Console.ReadLine();
 			n2 = int.Parse(tmp);
-			soma = n1 + n2;
-			Console.WriteLine("A soma vale: " + soma);//efetua soma
+			operacao = n1 + n2;
+			Console.WriteLine("A soma vale: " + operacao);//soma
+			operacao = n1 - n2;
+			Console.WriteLine("A diferença é: " + operacao);//subtração
 		}
 	}
 }
