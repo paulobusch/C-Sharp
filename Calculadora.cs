cff7e43c10a29891228ec2db31d777d5b1cb2a1e
Paulo <paulo202015@outlook.com.br>
2018-03-24 22:29:14 -0300

    Calculadora - Ajuste final

--- Calculadora.cs
+++ Calculadora.cs
@@ -11,13 +11,13 @@ namespace Calculadora{
 			tmp = Console.ReadLine();
 			n2 = int.Parse(tmp);
 			operacao = n1 + n2;//soma
-			Console.WriteLine("A soma vale: " + operacao);
+			Console.WriteLine("\nA soma vale: " + operacao);
 			operacao = n1 - n2;//subtração
 			Console.WriteLine("A diferença é: " + operacao);
 			operacao = n1 * n2;//multiplicação
 			Console.WriteLine("O produto é: " + operacao);
 			operacao = n1 / n2;//divisão
-			Console.WriteLine("A divisão é: " + operacao);
+			Console.WriteLine("A divisão é: " + operacao + "\n");
 		}
 	}
 }
