3ad1056b19ea303d4745a150b7e87f9c32aefab7
Paulo <paulo202015@outlook.com.br>
2018-03-25 00:36:35 -0300

    [Media notas] -> Captura de notas

--- Media_notas.cs
+++ Media_notas.cs
@@ -2,7 +2,15 @@ using System;
 namespace Media{
 	class Programa{
 		static void Main(String[] args){
-			
+			int som = 0;
+			Console.WriteLine("Digite a 1º nota: ");
+			som += Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Digite a 2º nota: ");
+			som += Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Digite a 3º nota: ");
+			som += Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Digite a 4º nota: ");
+			som += Convert.ToInt32(Console.ReadLine());
 		}
 	}
 }
