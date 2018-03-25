3ff036c81c7d8a4051795ddb246ab5c27946a8f7
Paulo <paulo202015@outlook.com.br>
2018-03-25 00:43:48 -0300

    [Media notas] -> Media e situação

--- Media_notas.cs
+++ Media_notas.cs
@@ -2,7 +2,8 @@ using System;
 namespace Media{
 	class Programa{
 		static void Main(String[] args){
-			int som = 0;
+			float som = 0, med;
+			string situacao = "";
 			Console.WriteLine("Digite a 1º nota: ");
 			som += Convert.ToInt32(Console.ReadLine());
 			Console.WriteLine("Digite a 2º nota: ");
@@ -11,6 +12,16 @@ namespace Media{
 			som += Convert.ToInt32(Console.ReadLine());
 			Console.WriteLine("Digite a 4º nota: ");
 			som += Convert.ToInt32(Console.ReadLine());
+			med = som/4;
+			Console.WriteLine("\nA media final foi......: " + med);
+			if(med>=7){
+				situacao = "APROVADO";
+			}else if(med>=5){
+				situacao = "EM RECUPERAÇÃO";
+			}else{
+				situacao = "REPROVADO";
+			}
+			Console.WriteLine( "O aluno está: " + situacao);
 		}
 	}
 }
