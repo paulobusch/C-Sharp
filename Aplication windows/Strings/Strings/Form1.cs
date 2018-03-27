20da46c0a9e08d70afa3f7888d05ad6d32bf7a62
Paulo <paulo202015@outlook.com.br>
2018-03-27 16:19:10 -0300

    [Strings] -> Metodo Substring()

--- Aplication windows/Strings/Strings/Form1.cs
+++ Aplication windows/Strings/Strings/Form1.cs
@@ -19,9 +19,13 @@ namespace Strings
         private void btn_executar_Click(object sender, EventArgs e)
         {
             string frase = "Este tipo de variável é alfanumérica";
+            /*
             lbl_texto.Text = frase + " (contém " + frase.Length + " letras).";
-            lbl_texto.Text += " \nMédodo ToUpper() [" + frase.ToUpper() + " ].";
-            lbl_texto.Text += " \nMédodo ToLower() [" + frase.ToLower() + " ].";
+            lbl_texto.Text += " \nMédodo ToUpper() [" + frase.ToUpper() + "].";
+            lbl_texto.Text += " \nMédodo ToLower() [" + frase.ToLower() + "].";
+            */
+            lbl_texto.Text += " \nMédodo Substring() [" + frase.Substring(5) + "].";
+            lbl_texto.Text += " \nMédodo Substring() [" + frase.Substring(5,4) + "].";
         }
     }
 }
