64428aa445492ac73683ab52b297ee2ef5039731
Paulo <paulo202015@outlook.com.br>
2018-03-27 15:34:33 -0300

    [Array] -> Vetor

--- Aplication windows/Array/Array/Form1.cs
+++ Aplication windows/Array/Array/Form1.cs
@@ -18,7 +18,17 @@ namespace Array
 
         private void btn_executar_Click(object sender, EventArgs e)
         {
-
+            /*
+            int[] valores;
+            valores = new int[5];
+            valores[0] = 10;
+            valores[1] = 10;
+            valores[2] = 10;
+            valores[3] = 10;
+            valores[4] = 10;
+            */
+            int[] valores = new int[]{10,20,30,40,50};
+            lst_array.Items.Add(valores[0]);
         }
     }
 }
