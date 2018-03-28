86c5c3f95b82e85309ebf0e46bfc2819850da363
Paulo <paulo202015@outlook.com.br>
2018-03-28 17:02:50 -0300

    [Classes] -> Novo objeto

---
+++ Aplication windows/Classes/Classes/pessoa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Classes
+{
+    class pessoa
+    {
+        public string nome;
+        public int idade;
+        public string corCabelo;
+    }
+}
