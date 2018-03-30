2dfa334fda7beb86643954d9ee32f064f962aaeb
Paulo <paulo202015@outlook.com.br>
2018-03-30 14:09:51 -0300

    [ClassesMetodos] -> Código gerado

---
+++ Aplication windows/ClassesMetodos/ClassesMetodos/cl_matematica.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassesMetodos
+{
+    class cl_matematica{
+        public int adicao(){
+
+        }
+    }
+}
