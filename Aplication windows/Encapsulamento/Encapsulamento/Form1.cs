4760e714ea50cc1c3cbb7606053384bdaa876bcc
Paulo <paulo202015@outlook.com.br>
2018-03-31 15:55:50 -0300

    [Encapsulamento] -> Calcula preço de produto

--- Aplication windows/Encapsulamento/Encapsulamento/Form1.cs
+++ Aplication windows/Encapsulamento/Encapsulamento/Form1.cs
@@ -18,7 +18,17 @@ namespace Encapsulamento
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            //apresenta o preço final do produto
+            cl_calculo calculadora = new cl_calculo();
+            int tipo_p;
+            if(rd_1.Checked){
+                tipo_p = 1;
+            }else if (rd_2.Checked){
+                tipo_p = 2;
+            }else{
+                tipo_p = 3;
+            }
+            MessageBox.Show("Preço final = " + calculadora.CalcPreco(Convert.ToInt16(txt_preco.Text), tipo_p));
         }
     }
 }
--- Aplication windows/Encapsulamento/Encapsulamento/cl_calculo.cs
+++ Aplication windows/Encapsulamento/Encapsulamento/cl_calculo.cs
@@ -7,5 +7,26 @@ namespace Encapsulamento
 {
     class cl_calculo
     {
+        private int p1 = 5;
+        private int p2 = 12;
+        private int p3 = 23;
+
+        public int CalcPreco(int preco_i, int tipo_p)
+        {
+            //calculo do preço final
+            switch (tipo_p)
+            {
+                case 1://alimento
+                    preco_i += (preco_i * p1 / 100);
+                    break;
+                case 2://objeto
+                    preco_i += (preco_i * p2 / 100);
+                    break;
+                case 3://higiene
+                    preco_i += (preco_i * p3 / 100);
+                    break;
+            }
+            return preco_i;
+        }
     }
 }
