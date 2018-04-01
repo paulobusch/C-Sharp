7756cd84605977edc784cf920f7bbb652aaf04ea
Paulo <paulo202015@outlook.com.br>
2018-04-01 20:01:48 -0300

    [DataHora] ->

---
+++ Aplication windows/DataHora/DataHora/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace DataHora
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_resultado = new System.Windows.Forms.Button();
+            this.lbl_resultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_resultado
+            // 
+            this.btn_resultado.Location = new System.Drawing.Point(469, 409);
+            this.btn_resultado.Name = "btn_resultado";
+            this.btn_resultado.Size = new System.Drawing.Size(98, 46);
+            this.btn_resultado.TabIndex = 0;
+            this.btn_resultado.Text = "Executar";
+            this.btn_resultado.UseVisualStyleBackColor = true;
+            this.btn_resultado.Click += new System.EventHandler(this.btn_resultado_Click);
+            // 
+            // lbl_resultado
+            // 
+            this.lbl_resultado.Location = new System.Drawing.Point(12, 9);
+            this.lbl_resultado.Name = "lbl_resultado";
+            this.lbl_resultado.Size = new System.Drawing.Size(555, 91);
+            this.lbl_resultado.TabIndex = 1;
+            this.lbl_resultado.Text = "Texto";
+            this.lbl_resultado.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 467);
+            this.Controls.Add(this.lbl_resultado);
+            this.Controls.Add(this.btn_resultado);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_resultado;
+        private System.Windows.Forms.Label lbl_resultado;
+    }
+}
+
---
+++ Aplication windows/DataHora/DataHora/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DataHora
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_resultado_Click(object sender, EventArgs e)
+        {
+            lbl_resultado.Text = DateTime.Now.ToString("yyyy-MM-dd");
+        }
+    }
+}
