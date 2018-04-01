6d6dc397bccbbd525ceed80ca747247bb4c2b25c
Paulo <paulo202015@outlook.com.br>
2018-04-01 12:12:29 -0300

    [Arquivos] -> Atualização de diretórios

---
+++ Aplication windows/ConstrutorOverloading/ConstrutorOverloading/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associada a um assembly.
+[assembly: AssemblyTitle("ConstrutorOverloading")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConstrutorOverloading")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("fa6df1b7-b4a6-4418-a16f-e0dcb943df4b")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// utilizando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Aplication windows/ConstrutorOverloading/ConstrutorOverloading/teste.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConstrutorOverloading
+{
+    class teste
+    {
+        string _nome;
+        int _idade;
+
+        public teste(string nome, int idade)
+        {
+            _nome = nome;
+            _idade = idade;
+        }
+
+        public void Apresentar()
+        {
+            System.Windows.Forms.MessageBox.Show("Nome: " + _nome + " idade: " + _idade);
+        }
+        public void Apresentar(string separador)
+        {
+            System.Windows.Forms.MessageBox.Show(_nome + separador + _idade);
+        }
+    }
+}
