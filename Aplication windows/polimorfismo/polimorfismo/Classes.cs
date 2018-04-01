2c18214332a166779cc4a17c5454d28505da0631
Paulo <paulo202015@outlook.com.br>
2018-03-31 23:27:52 -0300

    [Polimorfismo] -> Código implementado

--- Aplication windows/polimorfismo/polimorfismo/Classes.cs
+++ Aplication windows/polimorfismo/polimorfismo/Classes.cs
@@ -7,6 +7,59 @@ namespace polimorfismo
 {
     abstract class forma_geometrica
     {
+        int _largura;
+        int _altura;
 
+        public int Largura
+        {
+            get { return _largura; }
+            set { _largura = value; }
+        }
+
+        public int Altura
+        {
+            get { return _altura; }
+            set { _altura = value; }
+        }
+
+        public void Apresentar(){
+            Console.WriteLine("Este método pertence a classe base...");
+        }
+
+        public abstract void Desenhar();//será escrito na classe filho
+
+        public virtual void DesenharOutro()
+        {
+            Console.WriteLine("Método virtual da classe base");
+        }
+    }
+
+    class rectangulo : forma_geometrica
+    {
+        public override void Desenhar()
+        {
+            Console.WriteLine("Desenhar retângulo com as dimensões: " + Largura + " x " + Altura);
+        }
+
+        public override void DesenharOutro()
+        {
+            Console.WriteLine("Este é um metodo virtual substituído na classe rectangulo");
+        }
+    }
+
+    class triangulo : forma_geometrica
+    {
+        public override void Desenhar()
+        {
+            Console.WriteLine("Desenhar um triângulo com as dimensões: " + Largura + " x " + Altura);
+        }
+    }
+
+    class circunrefencia : forma_geometrica
+    {
+        public override void Desenhar()
+        {
+            Console.WriteLine("Desenhar uma circunferência com as dimensões: " + Largura + " x " + Altura);
+        }
     }
 }
--- Aplication windows/polimorfismo/polimorfismo/Form1.cs
+++ Aplication windows/polimorfismo/polimorfismo/Form1.cs
@@ -15,5 +15,29 @@ namespace polimorfismo
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            rectangulo forma1 = new rectangulo();
+            forma1.Largura = 50;
+            forma1.Altura = 100;
+            forma1.Apresentar();
+            forma1.Desenhar();
+            forma1.DesenharOutro();
+
+            triangulo forma2 = new triangulo();
+            forma2.Largura = 50;
+            forma2.Altura = 100;
+            forma2.Apresentar();
+            forma2.Desenhar();
+            forma2.DesenharOutro();
+
+            circunrefencia forma3 = new circunrefencia();
+            forma3.Largura = 50;
+            forma3.Altura = 100;
+            forma3.Apresentar();
+            forma3.Desenhar();
+            forma3.DesenharOutro();
+        }
     }
 }
