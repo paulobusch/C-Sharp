3246c212c700c98371881840be7f8c8f86552572
Paulo <paulo202015@outlook.com.br>
2018-04-01 12:38:50 -0300

    [Eventos] -> Código gerado

--- Aplication windows/Eventos/Eventos/Form1.Designer.cs
+++ Aplication windows/Eventos/Eventos/Form1.Designer.cs
@@ -28,13 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lbl_a = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(205, 276);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 65);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // lbl_a
+            // 
+            this.lbl_a.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_a.Location = new System.Drawing.Point(118, 115);
+            this.lbl_a.Name = "lbl_a";
+            this.lbl_a.Size = new System.Drawing.Size(292, 63);
+            this.lbl_a.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(537, 411);
+            this.Controls.Add(this.lbl_a);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lbl_a;
     }
 }
 
