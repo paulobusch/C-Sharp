3791ffa4d3c9a1589ba17d35aedafda643842b22
Paulo <paulo202015@outlook.com.br>
2018-04-02 13:57:45 -0300

    [ConversãoValores] -> Converções

--- Aplication windows/ConversãoValores/ConversãoValores/Form1.cs
+++ Aplication windows/ConversãoValores/ConversãoValores/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Windows.Forms;
 
 namespace ConversãoValores
@@ -18,7 +19,27 @@ namespace ConversãoValores
 
         private void btn_gravar_Click(object sender, EventArgs e)
         {
+            StreamWriter arquivo = new StreamWriter(@"C:\Users\paulo\Desktop\Projetos\C#\Aplication windows\ArquivosTexto\teste.txt", true, Encoding.Default);
+            
+            //string
+            arquivo.WriteLine("Esta frase é alfanumérica");
 
+            //int 
+            int valor = 100;
+            arquivo.WriteLine(valor);
+
+            //bool
+            bool resultado = false;
+            arquivo.WriteLine(resultado);
+
+            //data
+            arquivo.WriteLine(DateTime.Now);
+
+            //cor
+            Color cor = Color.Yellow;
+            arquivo.WriteLine(cor);
+
+            arquivo.Dispose();
         }
     }
 }
