c9af0dcb3aa5b14d59ad885e806c6d32b83e3d5e
Paulo <paulo202015@outlook.com.br>
2018-04-02 12:08:57 -0300

    [ArquivosTexto] -> Código gerado

---
+++ Aplication windows/ArquivosTexto/ArquivosTexto/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace ArquivosTexto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_executar = new System.Windows.Forms.Button();
+            this.lst_texto = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btn_executar
+            // 
+            this.btn_executar.Location = new System.Drawing.Point(286, 373);
+            this.btn_executar.Name = "btn_executar";
+            this.btn_executar.Size = new System.Drawing.Size(122, 47);
+            this.btn_executar.TabIndex = 0;
+            this.btn_executar.Text = "Executar";
+            this.btn_executar.UseVisualStyleBackColor = true;
+            this.btn_executar.Click += new System.EventHandler(this.btn_executar_Click);
+            // 
+            // lst_texto
+            // 
+            this.lst_texto.FormattingEnabled = true;
+            this.lst_texto.ItemHeight = 20;
+            this.lst_texto.Location = new System.Drawing.Point(12, 12);
+            this.lst_texto.Name = "lst_texto";
+            this.lst_texto.Size = new System.Drawing.Size(254, 404);
+            this.lst_texto.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(415, 436);
+            this.Controls.Add(this.lst_texto);
+            this.Controls.Add(this.btn_executar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_executar;
+        private System.Windows.Forms.ListBox lst_texto;
+    }
+}
+
---
+++ Aplication windows/ArquivosTexto/ArquivosTexto/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ArquivosTexto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_executar_Click(object sender, EventArgs e)
+        {
+            //gravar arquivo de texto
+            StreamWriter arquivo = new StreamWriter(@"C:\Users\paulo\Desktop\Projetos\C#\Aplication windows\ArquivosTexto\teste.txt", true, Encoding.Default);
+            arquivo.WriteLine("Frase de teste.");
+            arquivo.Dispose();//fecha e desocupa memoria
+        }
+    }
+}
