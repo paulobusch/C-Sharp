cc3c8559183b1fbf01e2931bcc46cf7d135b5408
Paulo <paulo202015@outlook.com.br>
2018-04-02 12:29:49 -0300

    [ArquivosTexto] -> Entrada e gravação de texto

--- Aplication windows/ArquivosTexto/ArquivosTexto/Form1.Designer.cs
+++ Aplication windows/ArquivosTexto/ArquivosTexto/Form1.Designer.cs
@@ -30,11 +30,12 @@
         {
             this.btn_executar = new System.Windows.Forms.Button();
             this.lst_texto = new System.Windows.Forms.ListBox();
+            this.txt_mensagem = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // btn_executar
             // 
-            this.btn_executar.Location = new System.Drawing.Point(286, 373);
+            this.btn_executar.Location = new System.Drawing.Point(281, 386);
             this.btn_executar.Name = "btn_executar";
             this.btn_executar.Size = new System.Drawing.Size(122, 47);
             this.btn_executar.TabIndex = 0;
@@ -48,19 +49,28 @@
             this.lst_texto.ItemHeight = 20;
             this.lst_texto.Location = new System.Drawing.Point(12, 12);
             this.lst_texto.Name = "lst_texto";
-            this.lst_texto.Size = new System.Drawing.Size(254, 404);
+            this.lst_texto.Size = new System.Drawing.Size(391, 304);
             this.lst_texto.TabIndex = 1;
+            // 
+            // txt_mensagem
+            // 
+            this.txt_mensagem.Location = new System.Drawing.Point(12, 340);
+            this.txt_mensagem.Name = "txt_mensagem";
+            this.txt_mensagem.Size = new System.Drawing.Size(391, 26);
+            this.txt_mensagem.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(415, 436);
+            this.ClientSize = new System.Drawing.Size(415, 445);
+            this.Controls.Add(this.txt_mensagem);
             this.Controls.Add(this.lst_texto);
             this.Controls.Add(this.btn_executar);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -68,6 +78,7 @@
 
         private System.Windows.Forms.Button btn_executar;
         private System.Windows.Forms.ListBox lst_texto;
+        private System.Windows.Forms.TextBox txt_mensagem;
     }
 }
 
