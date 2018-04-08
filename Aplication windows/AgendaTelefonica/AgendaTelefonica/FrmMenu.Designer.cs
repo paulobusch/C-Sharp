cde475fb2816783403c849cfd5775cbcacd8aa67
Paulo <paulo202015@outlook.com.br>
2018-04-08 11:54:24 -0300

    [AgendaTelefonica] -> Código gerado/componenetes

---
+++ Aplication windows/AgendaTelefonica/AgendaTelefonica/FrmMenu.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace AgendaTelefonica
+{
+    partial class FrmMenu
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_inserir_editar = new System.Windows.Forms.Button();
+            this.btn_pesquisar = new System.Windows.Forms.Button();
+            this.btn_sair = new System.Windows.Forms.Button();
+            this.lbl_versao = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(162, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "AGENDA";
+            // 
+            // btn_inserir_editar
+            // 
+            this.btn_inserir_editar.Location = new System.Drawing.Point(95, 74);
+            this.btn_inserir_editar.Name = "btn_inserir_editar";
+            this.btn_inserir_editar.Size = new System.Drawing.Size(288, 73);
+            this.btn_inserir_editar.TabIndex = 1;
+            this.btn_inserir_editar.Text = "Inserir / Editar";
+            this.btn_inserir_editar.UseVisualStyleBackColor = true;
+            this.btn_inserir_editar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_pesquisar
+            // 
+            this.btn_pesquisar.Location = new System.Drawing.Point(95, 172);
+            this.btn_pesquisar.Name = "btn_pesquisar";
+            this.btn_pesquisar.Size = new System.Drawing.Size(288, 73);
+            this.btn_pesquisar.TabIndex = 2;
+            this.btn_pesquisar.Text = "Pesquisar";
+            this.btn_pesquisar.UseVisualStyleBackColor = true;
+            this.btn_pesquisar.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btn_sair
+            // 
+            this.btn_sair.Location = new System.Drawing.Point(95, 272);
+            this.btn_sair.Name = "btn_sair";
+            this.btn_sair.Size = new System.Drawing.Size(288, 73);
+            this.btn_sair.TabIndex = 3;
+            this.btn_sair.Text = "Sair";
+            this.btn_sair.UseVisualStyleBackColor = true;
+            // 
+            // lbl_versao
+            // 
+            this.lbl_versao.Font = new System.Drawing.Font("Arial Narrow", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_versao.Location = new System.Drawing.Point(13, 391);
+            this.lbl_versao.Name = "lbl_versao";
+            this.lbl_versao.Size = new System.Drawing.Size(453, 23);
+            this.lbl_versao.TabIndex = 4;
+            this.lbl_versao.Text = "Apresenta versão";
+            this.lbl_versao.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FrmMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(478, 423);
+            this.Controls.Add(this.lbl_versao);
+            this.Controls.Add(this.btn_sair);
+            this.Controls.Add(this.btn_pesquisar);
+            this.Controls.Add(this.btn_inserir_editar);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "FrmMenu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Agenda 1.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_inserir_editar;
+        private System.Windows.Forms.Button btn_pesquisar;
+        private System.Windows.Forms.Button btn_sair;
+        private System.Windows.Forms.Label lbl_versao;
+    }
+}
+
---
+++ Aplication windows/AgendaTelefonica/AgendaTelefonica/FrmMenu.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AgendaTelefonica
+{
+    public partial class FrmMenu : Form
+    {
+        public FrmMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
