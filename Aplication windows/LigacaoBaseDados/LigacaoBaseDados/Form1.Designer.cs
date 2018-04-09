5cc919758f1ffb7a10319899f39c6a4c7b2f0a98
Paulo <paulo202015@outlook.com.br>
2018-04-08 21:55:09 -0300

    [SQL Compact Edition] -> Código gerado

---
+++ Aplication windows/LigacaoBaseDados/LigacaoBaseDados/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace LigacaoBaseDados
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_conectar = new System.Windows.Forms.Button();
+            this.lbl_resultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_conectar
+            // 
+            this.btn_conectar.Location = new System.Drawing.Point(279, 12);
+            this.btn_conectar.Name = "btn_conectar";
+            this.btn_conectar.Size = new System.Drawing.Size(128, 42);
+            this.btn_conectar.TabIndex = 0;
+            this.btn_conectar.Text = "Conectar";
+            this.btn_conectar.UseVisualStyleBackColor = true;
+            // 
+            // lbl_resultado
+            // 
+            this.lbl_resultado.AutoSize = true;
+            this.lbl_resultado.Location = new System.Drawing.Point(13, 12);
+            this.lbl_resultado.Name = "lbl_resultado";
+            this.lbl_resultado.Size = new System.Drawing.Size(82, 20);
+            this.lbl_resultado.TabIndex = 1;
+            this.lbl_resultado.Text = "Resultado";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(419, 98);
+            this.Controls.Add(this.lbl_resultado);
+            this.Controls.Add(this.btn_conectar);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_conectar;
+        private System.Windows.Forms.Label lbl_resultado;
+    }
+}
+
