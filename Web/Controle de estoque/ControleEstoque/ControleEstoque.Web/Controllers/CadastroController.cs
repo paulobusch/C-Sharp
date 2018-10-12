55b69204a884577cc778fbbbfb4af3585fe3e01e
Paulo Ricardo Busch <37111111+paulobusch@users.noreply.github.com>
2018-10-12 19:00:31 -0300

    files with project C Sharp web

---
+++ Web/Controle de estoque/ControleEstoque/ControleEstoque.Web/Controllers/CadastroController.cs
@@ -0,0 +1,182 @@
+﻿using ControleEstoque.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControleEstoque.Web.Controllers
+{
+    public class CadastroController : Controller
+    {
+
+        private const string _senhaPadrao = "{$127;$188}";
+
+        #region Usuários
+
+        [Authorize]
+        public ActionResult Usuario()
+        {
+            ViewBag.SenhaPadrao = _senhaPadrao;
+            return View(UsuarioModel.RecuperarLista());
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult RecuperarUsuario(int id)
+        {
+            return Json(UsuarioModel.RecuperarPeloId(id));
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult ExcluirUsuario(int id)
+        {
+            return Json(UsuarioModel.ExcluirPeloId(id));
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult SalvarUsuario(UsuarioModel model)
+        {
+            var res = "ok";
+            var msg = new List<string>();
+            var idSalvo = string.Empty;
+
+            if (!ModelState.IsValid)
+            {
+                res = "aviso";
+                msg = ModelState.Values.SelectMany(x => x.Errors).Select(x => x.ErrorMessage).ToList();
+            }
+            else
+            {
+                try
+                {
+                    if (model.Senha == _senhaPadrao)
+                        model.Senha = "";
+                    var id = model.Salvar();
+                    if (id > 0)
+                        idSalvo = id.ToString();
+                    else
+                        res = "erro";
+                }
+                catch (Exception ex)
+                {
+
+                    res = "erro";
+                }
+            }
+            return Json(new { Resultado = res, Mensagens = msg, IdSalvo = idSalvo });
+        }
+
+        #endregion
+
+        #region Grupo de produtos
+
+        [Authorize]
+        public ActionResult GrupoProduto()
+        {
+            return View(GrupoProdutoModel.RecuperarLista());
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult RecuperarGrupoProduto(int id)
+        {
+            return Json(GrupoProdutoModel.RecuperarPeloId(id));
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult ExcluirGrupoProduto(int id)
+        {
+            return Json(GrupoProdutoModel.ExcluirPeloId(id));
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult SalvarGrupoProduto(GrupoProdutoModel model)
+        {
+            var res = "ok";
+            var msg = new List<string>();
+            var idSalvo = string.Empty;
+
+            if (!ModelState.IsValid)
+            {
+                res = "aviso";
+                msg = ModelState.Values.SelectMany(x => x.Errors).Select(x => x.ErrorMessage).ToList();
+            }
+            else
+            {
+                try
+                {
+                    var id = model.Salvar();
+                    if(id > 0)
+                        idSalvo = id.ToString();
+                    else
+                        res = "erro";
+                }
+                catch (Exception ex)
+                {
+
+                    res = "erro";
+                }
+            }
+            return Json(new { Resultado = res, Mensagens = msg, IdSalvo = idSalvo});
+        }
+
+        #endregion
+
+        [Authorize]
+        public ActionResult MarcaProduto()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult LocalProduto()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult UnidadeMedida()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult Produto()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult Pais()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult Estado()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult Cidade()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult Fornecedor()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult PerfilUsuario()
+        {
+            return View();
+        }
+    }
+}
